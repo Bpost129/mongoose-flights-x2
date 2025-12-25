625f03067c444dd9c6f17ee68372f898bd62b101
Bpost129 <Bpost129@yahoo.com>
2025-12-25 00:17:32 -0500

    refactor server and router files to adjust for resource change, create Flight model

--- routes/flights.js
+++ routes/flights.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 const router = Router()
 
-// GET localhost:3000/users
+// GET localhost:3000/flights
 router.get('/', function(req, res) {
   res.send('respond with a resource')
 })
